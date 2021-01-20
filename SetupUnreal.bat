7852d9680276ae045f11b4de51531b35e6e99e0c
Paul Oliver <pauliver@users.noreply.github.com>
2021-01-20 06:50:58 -0800

    Create SetupUnreal.bat

---
+++ SetupUnreal.bat
@@ -0,0 +1,10 @@
+set EnginePath=%~dp0/
+setlocal
+pushd "%~dp0"
+git status
+git submodule init
+git submodule update
+cd ./UnrealEngine/
+call setup.bat --force 
+call GenerateProjectFiles.bat
+rem ./Engine/Build/BatchFiles/Build.bat Unreal Win64 Development "$(Build.SourcesDirectory)/Working/Unreal_Examples/Unreal.uproject" -WaitMutex -FromMsBuild 
